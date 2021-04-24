064985f491d1e1dee2773013de759964563644a8
amarp23 <78097052+amarp23@users.noreply.github.com>
2021-04-24 16:10:00 -0500

    changed grid x and o appear when space is clicked on

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-// Hi
+    public void startGame(View view){
+        Intent intent = new Intent(this, gameScreen.class);
+        startActivity(intent);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/tictactoe/gameScreen.java
+++ app/src/main/java/com/example/tictactoe/gameScreen.java
@@ -2,13 +2,53 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class gameScreen extends AppCompatActivity {
+    boolean gameActive = true;
+
+    int currentPlayer = 0;
+    int[] gameBoard = {2, 2, 2, 2, 2, 2, 2, 2, 2,};
+    // 0- X
+    // 1- O
+    // 2- empty
+
+    int squaresClicked = 0;
+    public void clicked(View view){
+        ImageView img = (ImageView) view;
+        int clickedImg = Integer.parseInt(img.getTag().toString());
+
+        if(gameBoard[clickedImg] == 2){
+            squaresClicked++;
+
+            if(squaresClicked == 9){
+                gameActive = false;
+            }
+
+            gameBoard[clickedImg] = currentPlayer;
+
+            if(currentPlayer == 0){
+                img.setImageResource(R.drawable.x_image);
+                currentPlayer = 1;
+            } else {
+                img.setImageResource(R.drawable.o_image);
+                currentPlayer = 0;
+            }
+        }
+    }
+
+    public void resetGame(View view){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_screen);
+
+        Intent intent = getIntent();
     }
 }
