cd6013aeb0a80564e051f1d242d031dbf33411b2
amarp23 <78097052+amarp23@users.noreply.github.com>
2021-05-02 22:28:53 -0500

    player1 sign up works tried to stop duplicate usernames but didn't work

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -5,14 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -34,7 +40,28 @@ public class MainActivity extends AppCompatActivity {
             toastMessage("Please enter a username");
             return;
         }
+        final boolean[] isNameTaken = {false};
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                if(username.toLowerCase() == document.getString("name").toLowerCase()){
+                                    toastMessage("This username is taken. Please enter a different one");
+                                    isNameTaken[0] = true;
+                                    return;
+                                }
+                            }
 
+                        } else {
+                            Log.w("MainActivity", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+        if(isNameTaken[0]){
+            return;
+        }
         Map<String, Object> newUser = new HashMap<String, Object>();
         newUser.put("name", username);
         newUser.put("win", 0);
@@ -46,13 +73,14 @@ public class MainActivity extends AppCompatActivity {
                 .addOnSuccessListener(new OnSuccessListener<DocumentReference>(){
                     @Override
                     public void onSuccess(DocumentReference documentReference){
-                        toastMessage("Run stored successfully");
+                        toastMessage("You are signed up!");
+                        usernameEditText.setEnabled(false);
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        toastMessage("Run failed to add");
+                        toastMessage("Failed to sign up");
                     }
                 });
     }
