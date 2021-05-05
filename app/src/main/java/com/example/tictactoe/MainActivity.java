2f869c4cc83491b5dc30b3559c707ae54473fb13
amarp23 <78097052+amarp23@users.noreply.github.com>
2021-05-05 09:45:42 -0500

    sign up works log in almost works

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -25,8 +25,11 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private EditText usernameEditText;
-
+    String player1;
+    String player2;
+    int i;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
 
     public void startGame(View view){
         Intent intent = new Intent(this, gameScreen.class);
@@ -34,45 +37,64 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void signUp(View view){
-        String username = usernameEditText.getText().toString();
+        String username;
+        if(view.getTag().toString().equals("1")){
+            usernameEditText = (EditText) findViewById(R.id.player1username);
+            i = 1;
+        } else {
+            usernameEditText = (EditText) findViewById(R.id.player2username);
+            i = 2;
+        }
+        username = usernameEditText.getText().toString();
 
+        checkSignUp(username);
+    }
+
+    public void checkSignUp(String username){
         if(username.isEmpty()){
             toastMessage("Please enter a username");
             return;
         }
-        final boolean[] isNameTaken = {false};
+
         db.collection("users")
+                .whereEqualTo("name".toLowerCase(), username.toLowerCase())
                 .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+               .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
                         if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                if(username.toLowerCase() == document.getString("name").toLowerCase()){
-                                    toastMessage("This username is taken. Please enter a different one");
-                                    isNameTaken[0] = true;
-                                    return;
-                                }
+                            if(task.getResult().isEmpty()) {
+                                addUser(username);
+                            }
+                            else {
+                                toastMessage("Username is already taken");
                             }
 
                         } else {
                             Log.w("MainActivity", "Error getting documents.", task.getException());
                         }
-                    }
+                   }
                 });
-        if(isNameTaken[0]){
-            return;
-        }
+
+
+    }
+
+    public void addUser(String username){
         Map<String, Object> newUser = new HashMap<String, Object>();
         newUser.put("name", username);
         newUser.put("win", 0);
         newUser.put("loss", 0);
         newUser.put("tie", 0);
-
+        if(i == 1){
+            player1 = username;
+        } else {
+            player2 = username;
+        }
         db.collection("users")
                 .add(newUser)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>(){
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                     @Override
-                    public void onSuccess(DocumentReference documentReference){
+                    public void onSuccess(DocumentReference documentReference) {
                         toastMessage("You are signed up!");
                         usernameEditText.setEnabled(false);
                     }
@@ -84,11 +106,58 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    public void logIn(View view){
+        String username;
+        if(view.getTag().toString().equals("1")){
+            usernameEditText = (EditText) findViewById(R.id.player1username);
+            i = 1;
+        } else {
+            usernameEditText = (EditText) findViewById(R.id.player2username);
+            i = 2;
+        }
+        username = usernameEditText.getText().toString();
+
+        checkLogIn(username);
+    }
+
+    public void checkLogIn(String username){
+        if(username.isEmpty()){
+            toastMessage("Please enter a username");
+            return;
+        }
+
+        db.collection("users")
+                .whereEqualTo("name".toLowerCase(), username.toLowerCase())
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        if (task.isSuccessful()) {
+                            if(!task.getResult().isEmpty()) {
+                                toastMessage("You are logged in!");
+                                if(i == 1){
+                                    player1 = username;
+                                } else {
+                                    player2 = username;
+                                }
+                            }
+                            else {
+                                toastMessage("Username not found");
+                            }
+
+                        } else {
+                            Log.w("MainActivity", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        usernameEditText = (EditText) findViewById(R.id.player1username);
     }
 
     private void toastMessage(String message){
